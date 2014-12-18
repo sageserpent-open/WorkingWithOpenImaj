900a44fd5ac82d89bfa071b75dfc59590f0670c4
gerardMurphy <gerardMurphy@.(none)>
2014-12-18 16:52:19 +0000

    Refactor to traverse the y-direction for each processing stage, this is to permit the implementation to be cutover to using the non-standard basis.

--- src/main/scala/com/sageSerpent/throwaway/vision/HaarWaveletProcessor.scala
+++ src/main/scala/com/sageSerpent/throwaway/vision/HaarWaveletProcessor.scala
@@ -16,66 +16,64 @@ object HaarWaveletProcessor {
       for (band <- 0 until image.numBands()) {
         trait TranspositionContext {
           val startX: Int
-          val y: Int
           val lengthOfSectionInXDirectionBeingProcessed: Int
+          val numberOfPixelsInYDirection: Int
           def gapBetweenWaveletCoefficientAndItsCorrespondingLowResolutionPixel = lengthOfSectionInXDirectionBeingProcessed / 2
-          def getImagePixel(x: Int): Float
-          def setImagePixel(x: Int, value: Float): Unit
+          def getImagePixel(x: Int, y: Int): Float
+          def setImagePixel(x: Int, y: Int, value: Float): Unit
 
           def subContext(): TranspositionContext // No need for F-bounds or 'this.type'.
 
           def hiveOffWaveletCoefficientsAlongSectionInXDirection() {
             if (1 < lengthOfSectionInXDirectionBeingProcessed) {
-              for (waveletCoefficientIndex <- 0 until gapBetweenWaveletCoefficientAndItsCorrespondingLowResolutionPixel) {
-                val lhsOfPairIndex = startX + 2 * waveletCoefficientIndex
-                val rhsOfPairIndex = 1 + lhsOfPairIndex
-                val lhs = getImagePixel(lhsOfPairIndex)
-                val rhs = getImagePixel(rhsOfPairIndex)
-                val waveletCoefficient = normalisation * 0.5 * (lhs - rhs)
-                val lowResolution = normalisation * 0.5 * (lhs + rhs)
-                val lowResolutionIndex = gapBetweenWaveletCoefficientAndItsCorrespondingLowResolutionPixel + waveletCoefficientIndex
-                mutableBufferForResultRowPixels(waveletCoefficientIndex) = waveletCoefficient.toFloat
-                mutableBufferForResultRowPixels(lowResolutionIndex) = lowResolution.toFloat
-              }
-              for (bufferIndex <- 0 until lengthOfSectionInXDirectionBeingProcessed) {
-                setImagePixel(startX + bufferIndex, mutableBufferForResultRowPixels(bufferIndex))
+              for (y <- 0 until numberOfPixelsInYDirection) {
+                for (waveletCoefficientIndex <- 0 until gapBetweenWaveletCoefficientAndItsCorrespondingLowResolutionPixel) {
+                  val lhsOfPairIndex = startX + 2 * waveletCoefficientIndex
+                  val rhsOfPairIndex = 1 + lhsOfPairIndex
+                  val lhs = getImagePixel(lhsOfPairIndex, y)
+                  val rhs = getImagePixel(rhsOfPairIndex, y)
+                  val waveletCoefficient = normalisation * 0.5 * (lhs - rhs)
+                  val lowResolution = normalisation * 0.5 * (lhs + rhs)
+                  val lowResolutionIndex = gapBetweenWaveletCoefficientAndItsCorrespondingLowResolutionPixel + waveletCoefficientIndex
+                  mutableBufferForResultRowPixels(waveletCoefficientIndex) = waveletCoefficient.toFloat
+                  mutableBufferForResultRowPixels(lowResolutionIndex) = lowResolution.toFloat
+                }
+                for (bufferIndex <- 0 until lengthOfSectionInXDirectionBeingProcessed) {
+                  setImagePixel(startX + bufferIndex, y, mutableBufferForResultRowPixels(bufferIndex))
+                }
               }
               subContext().hiveOffWaveletCoefficientsAlongSectionInXDirection()
             }
           }
         }
 
-        class NoTransposition(val y: Int, val startX: Int) extends TranspositionContext {
+        class NoTransposition(val startX: Int) extends TranspositionContext {
           val lengthOfSectionInXDirectionBeingProcessed: Int = image.getWidth() - startX
+          val numberOfPixelsInYDirection: Int = image.getHeight()
 
-          def getImagePixel(x: Int): Float = image.getBand(band).pixels(y)(x)
-          def setImagePixel(x: Int, value: Float) {
+          def getImagePixel(x: Int, y: Int): Float = image.getBand(band).pixels(y)(x)
+          def setImagePixel(x: Int, y: Int, value: Float) {
             image.getBand(band).pixels(y)(x) = value
           }
 
-          def subContext(): TranspositionContext = new NoTransposition(y,
-            gapBetweenWaveletCoefficientAndItsCorrespondingLowResolutionPixel + startX)
+          def subContext(): TranspositionContext = new NoTransposition(gapBetweenWaveletCoefficientAndItsCorrespondingLowResolutionPixel + startX)
         }
-        
-        class Transposition(val y: Int, val startX: Int) extends TranspositionContext {
-          val lengthOfSectionInXDirectionBeingProcessed: Int = image.getHeight() - startX
 
-          def getImagePixel(x: Int): Float = image.getBand(band).pixels(x)(y)
-          def setImagePixel(x: Int, value: Float) {
+        class Transposition(val startX: Int) extends TranspositionContext {
+          val lengthOfSectionInXDirectionBeingProcessed: Int = image.getHeight() - startX
+          val numberOfPixelsInYDirection: Int = image.getWidth()
+
+          def getImagePixel(x: Int, y: Int): Float = image.getBand(band).pixels(x)(y)
+          def setImagePixel(x: Int, y: Int, value: Float) {
             image.getBand(band).pixels(x)(y) = value
           }
 
-          def subContext(): TranspositionContext = new Transposition(y,
-            gapBetweenWaveletCoefficientAndItsCorrespondingLowResolutionPixel + startX)
-        }        
-
-        for (row <- 0 until image.getHeight()) {
-          new NoTransposition(row, 0).hiveOffWaveletCoefficientsAlongSectionInXDirection()
+          def subContext(): TranspositionContext = new Transposition(gapBetweenWaveletCoefficientAndItsCorrespondingLowResolutionPixel + startX)
         }
-        
-        for (column <- 0 until image.getWidth()) {
-          new Transposition(column, 0).hiveOffWaveletCoefficientsAlongSectionInXDirection()
-        }        
+
+        new NoTransposition(0).hiveOffWaveletCoefficientsAlongSectionInXDirection()
+
+        new Transposition(0).hiveOffWaveletCoefficientsAlongSectionInXDirection()
       }
     }
   }
